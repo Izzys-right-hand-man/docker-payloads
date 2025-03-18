1869bca818f38f83da18d17a505781bdfa172994
Izzys-right-hand-man <Ciii30@pcstudents.us>
2025-03-18 14:58:32 -0600

    Update wizard.py

--- wizard.py
+++ wizard.py
@@ -3,9 +3,11 @@ import sys
 import time
 import random
 
-payload = "
+
 
 
 addr = input("Target IPv4 address (ex: 10.10.10.10): ")
 
 prt = input("\n Target port enter for default(10000): ")
+
+payload = ""
