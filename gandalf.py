312af9eecf6cb378bf4376ed53b69f96532b9985
Izzys-right-hand-man <Ciii30@pcstudents.us>
2025-03-26 12:06:47 -0600

    Update gandalf.py

--- gandalf.py
+++ gandalf.py
@@ -2,7 +2,13 @@ import os
 import time
 import sys
 import random
+
+#including custom modules found in spells folder
+
+sys.path.append('/spells') # Add the directory to the Python path
+
 import python_payload
+
 load_names = ["python_payload.py","python.py","payload.py","python_payload","payload]","python_payload","python payload"]
 run = True
 banner = """
